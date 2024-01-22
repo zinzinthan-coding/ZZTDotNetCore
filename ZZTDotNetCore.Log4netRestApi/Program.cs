5d9088c83774ee81e78857983cb0b9b62e8ac296
Zakawar <thihathurazakawarmaung@gmail.com>
2024-01-22 23:50:34 +0630

    -

---
+++ ZZTDotNetCore.Log4netRestApi/Program.cs
@@ -0,0 +1,41 @@
+using log4net;
+using Microsoft.EntityFrameworkCore;
+using ZZTDotNetCore.Log4netRestApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.ClearProviders();
+builder.Logging.AddLog4Net();
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+},
+ServiceLifetime.Transient,
+ServiceLifetime.Transient
+);
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
